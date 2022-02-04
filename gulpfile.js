5cbb7b1b1458177bb3add7c92730f23e6f2a2dd5
rinhail <41772900+rinhail@users.noreply.github.com>
2022-02-04 15:38:47 +0300

    -libs

--- gulpfile.js
+++ gulpfile.js
@@ -37,13 +37,6 @@ gulp.task('styles', function() {
 
 gulp.task('scripts', function() {
 	return gulp.src([
-		'app/libs/jquery/dist/jquery.min.js',
-		'app/libs/menu/jquery.smartmenus.min.js',
-		'app/libs/fancybox/jquery.fancybox.min.js',
-		'app/libs/owlcarousel/owl.carousel.min.js',
-		'app/libs/jquery.lazy.min.js',
-		'app/libs/bootstrap/js/util.js',
-		'app/libs/bootstrap/js/tab.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
